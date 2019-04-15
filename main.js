c7991a5defc51d3f5d32762360f5ac6785b72716
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-15 16:48:55 -0500

    Added single page functionality to javascript file

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log('your js file is working');
+const home = document.getElementById('homeContainer');
+const bio = document.getElementById('bioContainer');
+const tech = document.getElementById('technologiesContainer');
+const projects = document.getElementById('projectsContainer');
+
+const homePageLoad = () => {
+    home.style.display = "block";
+    bio.style.display = "none";
+    tech.style.display = "none";
+    projects.style.display = "none";
+};
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+
+
+const navButtonAction = (e) => {
+    if (e.target.id === 'bioBtn') {
+        home.style.display = "none";
+        bio.style.display = "block";
+        tech.style.display = "none";
+        projects.style.display = "none";
+    } else if (e.target.id === 'techBtn') {
+        home.style.display = "none";
+        bio.style.display = "none";
+        tech.style.display = "block";
+        projects.style.display = "none";
+    } else if (e.target.id === 'projectsBtn') {
+        home.style.display = "none";
+        bio.style.display = "none";
+        tech.style.display = "none";
+        projects.style.display = "block";       
+    } else return
+};
+
+const eventListeners = () => {
+    document.getElementById('bioBtn').addEventListener('click', navButtonAction);
+    document.getElementById('techBtn').addEventListener('click', navButtonAction);
+    document.getElementById('projectsBtn').addEventListener('click', navButtonAction)
+};
+
+const init = () => {
+    homePageLoad();
+    eventListeners();
+};
+
+init();
